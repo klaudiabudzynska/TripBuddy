8479b5c4c49bc6bcdc63c55e0ca8ca776af1577d
Klaudia Budzyńska <klaudia.budzynska1@gmail.com>
2023-06-30 19:17:21 +0200

    filtering by categories

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,18 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { callProxy } from '../../helpers/fetch.ts';
 import LocationDetails from '../LocationDetails';
 import { LocationDetailsProps } from '../LocationDetails/typings.ts';
 
+enum Category {
+  hotel = 'hotels',
+  attraction = 'attractions',
+  restaurant = 'restaurants',
+  geos = 'geos',
+}
+
 const Search = () => {
   const [locationDetails, setLocationDetails] = useState<LocationDetailsProps>({});
+  const [category, setCategory] = useState<string>(Category.geos);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSearch = () => {
@@ -20,8 +28,12 @@ const Search = () => {
       .catch((err) => console.error('error:' + err));
   };
 
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value);
+  };
+
   const getLocationDetails = (locationId: string) => {
-    callProxy('/tripadvisor-api/location-details', { locationId })
+    callProxy('/tripadvisor-api/location-details', { locationId, category })
       .then((res) => {
         console.log(res);
         setLocationDetails(res);
@@ -33,6 +45,11 @@ const Search = () => {
     <>
       <input ref={inputRef} />
       <button onClick={handleSearch}>Search</button>
+      <select onChange={handleSelect}>
+        <option value={Category.hotel}>Hotel</option>
+        <option value={Category.attraction}>Attraction</option>
+        <option value={Category.restaurant}>Restaurant</option>
+      </select>
 
       <LocationDetails {...locationDetails} />
     </>
