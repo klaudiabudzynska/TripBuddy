f3761814b8a236ef12f77c470ebf1cb9cf4363b6
Klaudia Budzyńska <klaudia.budzynska1@gmail.com>
2023-10-19 20:26:43 +0200

    error page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './pages/Main';
 import TripPlan from './pages/TripPlan';
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/trip-plan',
