86d342d9f9cc43080c56203c4ad8eb94c21ad3b6
klaudiabudzynska <klaudia.budzynska@ringieraxelspringer.pl>
2023-10-25 20:53:17 +0200

    adding trip to list

--- src/pages/TripPlan/index.tsx
+++ src/pages/TripPlan/index.tsx
@@ -1,10 +1,16 @@
+import React, {useState} from 'react';
 import Button from '../../components/Button';
 import Modal from '../../components/Modal';
 import styles from './index.module.scss';
-import {useState} from 'react';
 
 function TripPlan() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [trips, setTrips] = useState<string[]>([]);
+  const [newTripName, setNewTripName] = useState('');
+
+  const onTripNameInput = (e: React.FormEvent<HTMLInputElement>) => {
+    setNewTripName(e.currentTarget.value);
+  };
 
   const showAddingDialog = () => {
     setIsModalOpen(!isModalOpen);
@@ -16,15 +22,21 @@ function TripPlan() {
 
   const saveTrip = () => {
     setIsModalOpen(false);
+    setTrips([...trips, newTripName]);
   };
 
   return <div>
     <h1 className={styles.title}>Your trip plans</h1>
     <Button value="Create your trip" onClick={showAddingDialog} />
+    <div>
+      {trips.map((trip, key) => {
+        return <p key={`trip-id-${key}`}>{trip}</p>;
+      })}
+    </div>
     <Modal isOpen={isModalOpen} title="Create a trip" closeModal={cancelAddingDialog} acceptAction={saveTrip}>
       <>
         <p>Trip name</p>
-        <input className={styles.input}/>
+        <input className={styles.input} onChange={onTripNameInput}/>
       </>
     </Modal>
   </div>;
