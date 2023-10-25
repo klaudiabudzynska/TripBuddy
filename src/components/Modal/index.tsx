b25e30a35be9718663379e1fee6c99e845271654
klaudiabudzynska <klaudia.budzynska@ringieraxelspringer.pl>
2023-10-25 21:10:36 +0200

    modal component changes

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,23 +3,23 @@ import styles from './index.module.scss';
 import Button, {ButtonStyle} from '../Button';
 
 type ModalProps = {
-    isOpen: boolean;
-    title?: string;
-    closeModal: MouseEventHandler<HTMLButtonElement>;
-    acceptAction?: MouseEventHandler<HTMLButtonElement>;
-    children: ReactElement;
+  isOpen: boolean;
+  title?: string;
+  closeModal: MouseEventHandler<HTMLButtonElement>;
+  acceptAction?: MouseEventHandler<HTMLButtonElement>;
+  children: ReactElement;
 }
 
 const Modal = ({ isOpen, title, closeModal, acceptAction, children }: ModalProps) => {
-  const ref: LegacyRef<HTMLDialogElement> | undefined = useRef(null);
+  const modalRef: LegacyRef<HTMLDialogElement> | undefined = useRef(null);
 
   useEffect(() => {
-    isOpen ? ref.current?.show() : ref.current?.close();
+    isOpen ? modalRef.current?.show() : modalRef.current?.close();
   }, [isOpen]);
 
   return (
     <dialog
-      ref={ref}
+      ref={modalRef}
       className={styles.modal}
     >
       <h2 className={styles.title}>{title}</h2>
